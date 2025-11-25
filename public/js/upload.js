c4edc555d6aab7b0bc1e3f405972f80aebafd377
Velocity <115943737+DasVelocity@users.noreply.github.com>
2025-11-25 20:27:31 +0100

    Update upload.js

--- public/js/upload.js
+++ public/js/upload.js
@@ -1,10 +1,10 @@
-import { ref as storageRef, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-storage.js";
+// helper to upload images/videos
 import { storage } from "./firebase.js";
+import { ref as sRef, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-storage.js";
 
-export async function uploadFile(file) {
-  if (!file) return null;
-  const ext = file.name.split('.').pop();
-  const fileRef = storageRef(storage, `media/${Date.now()}_${Math.random().toString(36)}.${ext}`);
-  await uploadBytes(fileRef, file);
-  return await getDownloadURL(fileRef);
+export async function uploadFile(file, pathPrefix = "uploads") {
+  const refStr = `${pathPrefix}/${Date.now()}_${file.name}`;
+  const s = sRef(storage, refStr);
+  await uploadBytes(s, file);
+  return await getDownloadURL(s);
 }
