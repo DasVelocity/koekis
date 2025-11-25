6e57b5b76abd81499a943bcc7a5d59730106084e
Velocity <115943737+DasVelocity@users.noreply.github.com>
2025-11-25 21:02:22 +0100

    Update supabase.js

--- public/js/supabase.js
+++ public/js/supabase.js
@@ -1,16 +1,4 @@
 const supabaseUrl = 'https://xbfwmzcsnloptzqcyftdk.supabase.co';
 const supabaseKey = 'sb_publishable_JsXdy0x7-bt02y83sVyFyQ_FI55P9bB';
-
-function initSupabase() {
-  if (typeof Supabase === 'undefined') {
-    setTimeout(initSupabase, 100);  
-    return;
-  }
-  
-  const { createClient } = Supabase;
-  window.supabase = createClient(supabaseUrl, supabaseKey);  
-}
-
-initSupabase();
-
-export const supabase = window.supabase; 
+const { createClient } = Supabase;
+export const supabase = createClient(supabaseUrl, supabaseKey);
